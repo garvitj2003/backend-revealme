32bdc1df7958e7fc9294072b08122fbfc4aac3f0
Garvit <garvitj2003@gmail.com>
2025-03-16 20:16:44 +0530

    changed quizId to UUID

--- prisma/migrations/20250308190539_added_stats_table/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "stats" (
-    "id" SERIAL NOT NULL,
-    "total_submission" INTEGER NOT NULL DEFAULT 0,
-
-    CONSTRAINT "stats_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20250316125230_initial/migration.sql
+++ prisma/migrations/20250316125230_initial/migration.sql
@@ -1,9 +1,17 @@
 -- CreateTable
+CREATE TABLE "rawQuiz" (
+    "id" SERIAL NOT NULL,
+    "creatorId" TEXT NOT NULL,
+
+    CONSTRAINT "rawQuiz_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Quiz" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "creatorId" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
+    "title" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Quiz_pkey" PRIMARY KEY ("id")
@@ -11,13 +19,24 @@ CREATE TABLE "Quiz" (
 
 -- CreateTable
 CREATE TABLE "Question" (
-    "id" TEXT NOT NULL,
+    "id" SERIAL NOT NULL,
     "quizId" TEXT NOT NULL,
     "text" TEXT NOT NULL,
     "options" TEXT[],
-    "correctAnswer" TEXT NOT NULL,
+    "correctAnswer" TEXT,
 
     CONSTRAINT "Question_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "rawQuestion" (
+    "id" SERIAL NOT NULL,
+    "creatorId" TEXT NOT NULL,
+    "selected" BOOLEAN NOT NULL,
+    "text" TEXT NOT NULL,
+    "options" TEXT[],
+
+    CONSTRAINT "rawQuestion_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -31,8 +50,25 @@ CREATE TABLE "LeaderboardEntry" (
     CONSTRAINT "LeaderboardEntry_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "stats" (
+    "id" SERIAL NOT NULL,
+    "total_submission" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "stats_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "rawQuiz_creatorId_key" ON "rawQuiz"("creatorId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "LeaderboardEntry_quizId_key" ON "LeaderboardEntry"("quizId");
+
 -- AddForeignKey
 ALTER TABLE "Question" ADD CONSTRAINT "Question_quizId_fkey" FOREIGN KEY ("quizId") REFERENCES "Quiz"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "rawQuestion" ADD CONSTRAINT "rawQuestion_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "rawQuiz"("creatorId") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "LeaderboardEntry" ADD CONSTRAINT "LeaderboardEntry_quizId_fkey" FOREIGN KEY ("quizId") REFERENCES "Quiz"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
